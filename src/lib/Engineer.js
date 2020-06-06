85caaf9fc3f0309f82372f8cb21bd5270e69e8e5
Stephanie Morrissey <sgmorrissey098@gmail.com>
2020-06-06 20:33:59 +0100

    Added extends from Employee to relevant classes and started to add prompts in app.js

--- src/lib/Engineer.js
+++ src/lib/Engineer.js
@@ -1,6 +1,8 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
-class Engineer {
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
   constructor(name, id, email, github) {
+    super(name, id, email);
     this.github = github;
   }
   getGithub() {
--- src/lib/Intern.js
+++ src/lib/Intern.js
@@ -1,6 +1,8 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
-class Intern {
+const Employee = require("./Employee");
+
+class Intern extends Employee {
   constructor(name, email, id, school) {
+    super(name, email, id);
     this.school = school;
   }
   getRole() {
